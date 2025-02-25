dda00d241625fa462514a445fe3a27b7c5465f81
yumu <93029942+yumunet@users.noreply.github.com>
2025-02-25 12:54:03 +0900

    feat: 空のプラグインを作成

---
+++ src/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using Wodistant.PluginLibrary.Hook.Keyboard;
+using Wodistant.PluginLibrary.MenuBar;
+
+namespace WodistantListFit
+{
+    public class Plugin : Wodistant.PluginLibrary.PluginBase
+    {
+        public override string Name => "ListFit";
+
+        public override string Author => "yumu";
+
+        public override string Version => "1.0.0";
+
+        public override string Description => "ドロップダウンリストの横幅を自動的に調整します。";
+
+        public override MenuBarInfo ManuBarInfomation
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public override IKeyboardAction[] KeyboardActions => new IKeyboardAction[]
+        {
+        };
+    }
+}
